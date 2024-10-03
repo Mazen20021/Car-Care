2c62ac7e9c42fcbd6f3c4d3905de537054169b03
Sharaf M. Mansour <55330747+Sharaf-Mansour@users.noreply.github.com>
2024-10-03 15:23:06 +0300

    Optional Params

--- lib/Components/Buttons.dart
+++ lib/Components/Buttons.dart
@@ -1,3 +1,4 @@
+import 'package:carcare/Config/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,7 +10,7 @@ class ButtonClass extends StatelessWidget {
   final double rightPadding;
   final double topPadding;
   final double bottomPadding;
-  final List<Color> gradientColors;
+  final List<Color>? gradientColors;
   final bool hasGradient;
   final bool hasImage;
   final Color normalColor;
@@ -18,70 +19,70 @@ class ButtonClass extends StatelessWidget {
   final Alignment begin;
   final Alignment end;
   final String label;
-  final double fontSize;
+  final double? fontSize;
   final Color textColor;
-  final VoidCallback onPressed;
-
+  final VoidCallback? onPressed;
   ButtonClass({
-    required this.width,
-    required this.height,
-    required this.borderRadius,
-    required this.leftPadding,
-    required this.rightPadding,
-    required this.topPadding,
-    required this.bottomPadding,
-    required this.gradientColors,
-    required this.normalColor,
-    required this.hasGradient,
-    required this.hasImage,
-    required this.imagePath,
-    required this.imageOpacity,
-    required this.begin,
-    required this.end,
-    required this.label,
-    required this.fontSize,
-    required this.textColor,
-    required this.onPressed,
+    this.width = 0,
+    this.height = 0,
+    this.borderRadius = 0,
+    this.leftPadding = 0,
+    this.rightPadding = 0,
+    this.topPadding = 0,
+    this.bottomPadding = 0,
+    this.gradientColors = null,
+    this.normalColor = Colors.transparent,
+    this.hasGradient = false,
+    this.hasImage = false,
+    this.imagePath = "",
+    this.imageOpacity = 1,
+    this.begin = Alignment.centerLeft,
+    this.end = Alignment.centerRight,
+    this.label = "",
+    this.fontSize = null,
+    this.textColor = MainColors.textWhite,
+    this.onPressed = null,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-
-      onTap: onPressed,
-      child: Padding(padding:EdgeInsets.fromLTRB(leftPadding,topPadding,rightPadding,bottomPadding),
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          width: width,
-          height: height,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(borderRadius),
-          image: hasImage
-              ? DecorationImage(
-            image: AssetImage(imagePath),
-            opacity: imageOpacity,
-            fit: BoxFit.cover,
-          )
-              : null,
-          gradient: hasGradient
-              ? LinearGradient(
-            colors: gradientColors,
-            begin: begin,
-            end: end,
-          )
-              : null,
-        ),
-        color: hasGradient? null: normalColor,
-        alignment: Alignment.center,
-        child: Text(
-          label,
-          style: GoogleFonts.rubik(
-            color: textColor,
-            fontSize: fontSize,
-            fontWeight: FontWeight.bold,
+        onTap: onPressed,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(
+              leftPadding, topPadding, rightPadding, bottomPadding),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            width: width,
+            height: height,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(borderRadius),
+              image: hasImage
+                  ? DecorationImage(
+                      image: AssetImage(imagePath),
+                      opacity: imageOpacity,
+                      fit: BoxFit.cover,
+                    )
+                  : null,
+              gradient: hasGradient
+                  ? LinearGradient(
+                      colors: gradientColors?.toList() ?? [],
+                      begin: begin,
+                      end: end,
+                    )
+                  : null,
+            ),
+            color: hasGradient ? null : normalColor,
+            alignment: Alignment.center,
+            child: Text(
+              label,
+              style: GoogleFonts.rubik(
+                color: textColor,
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
