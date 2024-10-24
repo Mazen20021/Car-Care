5a6b2e7572dd93628e4b28e23afd4fd693560382
Mazen Ahmed <mazen20190441@gmail.com>
2024-10-24 04:54:18 +0300

    Added Repair Data List

---
+++ lib/Config/repair_list_data.dart
@@ -0,0 +1,13 @@
+class RepairItem {
+  String name;
+  String date;
+  String km;
+  String cost;
+
+  RepairItem({
+    required this.name,
+    required this.date,
+    required this.km,
+    required this.cost,
+  });
+}
